83b9961517191e05bdb291ceb572fd04ad9ee638
kms152000 <kms152000@naver.com>
2025-09-17 02:39:00 +0900

    3장

---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @GetMapping("/signup")
+    public String signUpPage() {
+        return "members/new";
+    }
+
+    @PostMapping("/join")
+    public String join(MemberForm memberForm) {
+        Member member = memberForm.toEntity();
+        Member saved = memberRepository.save(member);
+
+        return "";
+    }
+}
