fb78ca516efbb3c58232f6c5fc70a446b4825e42
kms152000 <kms152000@naver.com>
2025-09-22 12:58:32 +0900

    5장

--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class Member {
     @Id
@@ -16,19 +22,4 @@ public class Member {
 
     @Column
     String password;
-
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
